8770b4b8dd217076694061ff9ee831b4b76ae8c6
davidmcdonagh <davidnmcdonagh@gmail.com>
2023-05-10 15:45:13 +0100

    Refactored code. Added parsing for reflection tables.

--- src/js/experiment.js
+++ src/js/experiment.js
@@ -1,35 +1,238 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { Line2 } from 'three/examples/jsm/lines/Line2.js';
-import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial';
-import { LineGeometry } from 'three/examples/jsm/lines/LineGeometry';
 import gsap from "gsap";
 import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'three.meshline';
+import {decode} from "msgpack-lite";
+
+class ExptParser{
+
+	constructor(){
+		this.exptJSON = null;
+	}
+
+	hasExptJSON(){
+		return this.exptJSON != null;
+	}
+
+	static isDIALSExpt(file, content){
+		const fileExt = file.name.split(".").pop() ;
+		if (fileExt === "expt" && content[0] === "{"){
+			return true;
+		}
+		return false;
+	}
+
+	parseExperiment = (file) => {
+		const reader = new FileReader();
+
+		return new Promise((resolve, reject) => {
+			reader.onerror = () => {
+				reader.abort();
+				reject(new DOMException("Problem parsing input file."));
+			};
+
+			reader.onloadend = () => {
+				resolve(reader.result);
+				if (ExptParser.isDIALSExpt(file, reader.result)){
+					this.exptJSON = JSON.parse(reader.result);
+				}
+			};
+			reader.readAsText(file);    
+		});
+	};
+
+	getDetectorPanelData(){
+		return this.exptJSON["detector"][0]["panels"];
+	}
+
+	getBeamData(){
+		return this.exptJSON["beam"][0];
+	}
+
+	getBeamDirection(){
+		const beamData = this.getBeamData();
+		return new THREE.Vector3(
+			beamData["direction"][0], 
+			beamData["direction"][1], 
+			beamData["direction"][2]
+		);
+	}
+
+	getNumDetectorPanels(){
+		return this.getDetectorPanelData().length;
+	}
+
+	getDetectorPanelName(idx){
+		return this.getDetectorPanelData()[idx]["name"];
+	}
+
+	getDetectorPanelCorners(idx){
+		const panelData = this.getDetectorPanelData()[idx];
+		var pxSize = new THREE.Vector2(panelData["pixel_size"][0], panelData["pixel_size"][1]);
+		var pxs = new THREE.Vector2(panelData["image_size"][0], panelData["image_size"][1]);
+		var panelSize = new THREE.Vector2(pxSize.x*pxs.x, pxSize.y*pxs.y);
+		var fa = new THREE.Vector3(panelData["fast_axis"][0], panelData["fast_axis"][1], panelData["fast_axis"][2]).multiplyScalar(panelSize.x);
+		var sa = new THREE.Vector3(panelData["slow_axis"][0], panelData["slow_axis"][1], panelData["slow_axis"][2]).multiplyScalar(panelSize.y);
+		var o = new THREE.Vector3(panelData["origin"][0], panelData["origin"][1], panelData["origin"][2]);
+
+		// Corners
+		var c1 = o.clone();
+		var c2 = o.clone().add(fa);
+		var c3 = o.clone().add(fa).add(sa);
+		var c4 = o.clone().add(sa);
+		return [c1, c2, c3, c4];
+	}
+
+}
+
+class ReflParser{
+
+	constructor(){
+		this.refl = null;
+	}
+
+	parseReflectionTable = (file) => {
+		const reader = new FileReader();
+
+		return new Promise((resolve, reject) => {
+			reader.onerror = () => {
+				reader.abort();
+				reject(new DOMException("Problem parsing input file."));
+			};
+
+			reader.onloadend = () => {
+				resolve(reader.result);
+				const decoded = decode(Buffer.from(reader.result));
+				this.refl = decoded[2]["data"];
+			};
+			reader.readAsArrayBuffer(file);    
+		});
+	};
+
+	get_reflection_table_buffer(reflection_table, column_name){
+		return buffer = reflection_table[2]["data"][column_name][1][1];
+	}
+
+	get_double_array(reflection_table, column_name){
+		const buffer = get_reflection_table_buffer(reflection_table, column_name);
+		const arr = new Float64Array(buffer.length/8);
+		let count = 0;
+		for (let i = 0; i < buffer.length; i+=8) {
+		arr[count] = buffer.readDoubleLE(i);
+		count++;
+		}
+		return arr;
+	};
+
+	get_vec3_double_array(reflection_table, column_name){
+		const buffer = get_reflection_table_buffer(reflection_table, column_name);
+		const arr = new Array(buffer.length/(8*3));
+		let count = 0;
+		for (let i = 0; i < buffer.length; i+=24){
+			vec = new Float64Array(3);
+			vec[0] = buffer.readDoubleLE(i);
+			vec[1] = buffer.readDoubleLE(i+8);
+			vec[2] = buffer.readDoubleLE(i+16);
+			arr[count] = vec;
+			count++;
+		}
+		return arr;
+	}
+}
+
 
 class ExperimentViewer{
 	constructor(){
-		this.setup();
+		this.setupScene();
+		this.expt = new ExptParser();
+		this.refl = new ReflParser();
+		this.tooltip = window.document.getElementById("tooltip");
 		window.renderer.setAnimationLoop(this.animate);
 	}
 
-	addExperiment(exptJSON){
-		this.exptJSON = exptJSON;
-		this.panelData = this.getDetectorPanels(exptJSON);
-		for (var i = 0; i < this.panelData.length; i++){
-			this.addDetectorPanelOutline(this.panelData[i]);
+	addExperiment = async (file) => {
+		await this.expt.parseExperiment(file);
+		for (var i = 0; i < this.expt.getNumDetectorPanels(); i++){
+			this.addDetectorPanelOutline(i);
 		}
-		this.beamData = this.getBeam(exptJSON);
-		this.addBeam(this.beamData);
+		this.addBeam();
 		this.setCameraToDefaultPosition();
-
 	}
 
-	setup(){
+	addReflectionTable = async (file) => {
+		await this.refl.parseReflectionTable(file);
+	}
+
+	setupScene(){
+		window.renderer = new THREE.WebGLRenderer();
 		window.renderer.setClearColor(ExperimentViewer.colors()["background"]);
-	}
+		window.renderer.setSize(window.innerWidth, window.innerHeight);
+		document.body.appendChild(window.renderer.domElement);
+		tooltip = window.document.getElementById("tooltip")
+		window.scene = new THREE.Scene()
+		window.scene.fog = new THREE.Fog(ExperimentViewer.colors()["background"], 500, 3000);
+		window.camera = new THREE.PerspectiveCamera(
+			45,
+			window.innerWidth / window.innerHeight,
+			0.0001,
+			10000
+		);
+		window.renderer.render(window.scene, window.camera);
+		window.rayCaster = new THREE.Raycaster();
 
-	static isDIALSExpt(fileString){
-		return (fileString[0] === '{');
+		// Controls
+		window.controls = new OrbitControls(window.camera, window.renderer.domElement);
+		window.controls.maxDistance = 3000;
+		window.controls.enablePan = false;
+		window.controls.enableDamping = true;
+		window.controls.dampingFactor = 0.1;
+		window.controls.update();
+
+		// Events
+		window.mousePosition = new THREE.Vector2();
+		window.addEventListener("mousemove", function (e) {
+			window.mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
+			window.mousePosition.y = - (e.clientY / window.innerHeight) * 2 + 1;
+		});
+
+		window.addEventListener("resize", function() {
+			window.camera.aspect = window.innerWidth / window.innerHeight;
+			window.camera.updateProjectionMatrix();
+			window.renderer.setSize(window.innerWidth, window.innerHeight);
+		});
+
+		window.addEventListener("dragstart", (event) => {
+			dragged = event.target;
+		});
+
+		window.addEventListener("dragover", (event) => {
+			event.preventDefault();
+		});
+
+		window.addEventListener('drop', function(event) {
+
+			event.preventDefault();
+			event.stopPropagation();
+			const file = event.dataTransfer.files[0];
+			const fileExt = file.name.split(".").pop();
+			if (fileExt == "refl"){
+				window.viewer.addReflectionTable(file);
+			}
+			else if (fileExt == "expt"){
+				window.viewer.addExperiment(file);
+			}
+		});
+
+		window.addEventListener('dblclick', function(event){
+			var pos = ExperimentViewer.getClickedPanelPos();
+			ExperimentViewer.rotateToPos(pos);
+		});
+
+		window.addEventListener('mousedown', function(event){
+			if (event.button == 2) { 
+				ExperimentViewer.rotateToPos(ExperimentViewer.cameraPositions()["default"]);
+			}
+		});
 	}
 
 	static colors(){
@@ -49,33 +252,9 @@ class ExperimentViewer{
 		};
 	}
 
-	getDetectorPanels(exptJSON){
-		return exptJSON["detector"][0]["panels"];
-	}
+	addDetectorPanelOutline(idx){
 
-	getBeam(exptJSON){
-		return exptJSON["beam"][0];
-	}
-
-	getPanelCorners(panelData){
-		var pxSize = new THREE.Vector2(panelData["pixel_size"][0], panelData["pixel_size"][1]);
-		var pxs = new THREE.Vector2(panelData["image_size"][0], panelData["image_size"][1]);
-		var panelSize = new THREE.Vector2(pxSize.x*pxs.x, pxSize.y*pxs.y);
-		var fa = new THREE.Vector3(panelData["fast_axis"][0], panelData["fast_axis"][1], panelData["fast_axis"][2]).multiplyScalar(panelSize.x);
-		var sa = new THREE.Vector3(panelData["slow_axis"][0], panelData["slow_axis"][1], panelData["slow_axis"][2]).multiplyScalar(panelSize.y);
-		var o = new THREE.Vector3(panelData["origin"][0], panelData["origin"][1], panelData["origin"][2]);
-
-		// Corners
-		var c1 = o.clone();
-		var c2 = o.clone().add(fa);
-		var c3 = o.clone().add(fa).add(sa);
-		var c4 = o.clone().add(sa);
-		return [c1, c2, c3, c4];
-	}
-
-	addDetectorPanelOutline(panelData){
-
-		var corners = this.getPanelCorners(panelData);
+		var corners = this.expt.getDetectorPanelCorners(idx);
 		corners.push(corners[0]);
 
 		const planeGeometry = new THREE.PlaneGeometry(192, 192);
@@ -85,7 +264,7 @@ class ExperimentViewer{
 			transparent: true,
 		});
 		const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-		plane.name = panelData["name"];
+		plane.name = this.expt.getDetectorPanelName(idx);
 
 		window.scene.add(plane);
 		var count = 0;
@@ -109,9 +288,9 @@ class ExperimentViewer{
 
 	}
 
-	addBeam(beamData){
+	addBeam(){
 		var beam_length = 2000.;
-		var bd = new THREE.Vector3(beamData["direction"][0], beamData["direction"][1], beamData["direction"][2]);
+		var bd = this.expt.getBeamDirection();;
 
 		var incidentVertices = []
 		incidentVertices.push(
@@ -169,7 +348,7 @@ class ExperimentViewer{
 	}
 
 	static displayName(name){
-		console.log(name)
+		tooltip.textContent = name;
 	}
 
 	static highlightObject(obj){
@@ -190,6 +369,9 @@ class ExperimentViewer{
 			}
 			*/
 			ExperimentViewer.highlightObject();
+		}
+		else{
+			ExperimentViewer.displayName(null);
 		}
 	}
 
@@ -234,78 +416,11 @@ class ExperimentViewer{
 		window.controls.update();
 		window.renderer.render(window.scene, window.camera);
 	}
-}
 
-/* Global Setup */
-
-window.renderer = new THREE.WebGLRenderer();
-window.renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(window.renderer.domElement);
-
-window.scene = new THREE.Scene()
-window.scene.fog = new THREE.Fog(ExperimentViewer.colors()["background"], 500, 3000);
-window.camera = new THREE.PerspectiveCamera(
-	45,
-	window.innerWidth / window.innerHeight,
-	0.0001,
-	10000
-);
-window.renderer.render(window.scene, window.camera);
-window.rayCaster = new THREE.Raycaster();
-
-// Controls
-window.controls = new OrbitControls(window.camera, window.renderer.domElement);
-window.controls.maxDistance = 3000;
-window.controls.enablePan = false;
-window.controls.enableDamping = true;
-window.controls.dampingFactor = 0.1;
-window.controls.update();
-
-// Events
-window.mousePosition = new THREE.Vector2();
-window.addEventListener("mousemove", function (e) {
-	window.mousePosition.x = (e.clientX / window.innerWidth) * 2 - 1;
-	window.mousePosition.y = - (e.clientY / window.innerHeight) * 2 + 1;
-});
-
-window.addEventListener("resize", function() {
-	window.camera.aspect = window.innerWidth / window.innerHeight;
-	window.camera.updateProjectionMatrix();
-	window.renderer.setSize(window.innerWidth, window.innerHeight);
-});
-
-window.addEventListener("dragstart", (event) => {
-  dragged = event.target;
-});
-
-window.addEventListener("dragover", (event) => {
-  event.preventDefault();
-});
-
-window.addEventListener('drop', function(event) {
-
-	var reader = new FileReader();
-	reader.onloadend = function() {
-		if (ExperimentViewer.isDIALSExpt(this.result)){
-			var data = JSON.parse(this.result);
-			window.viewer.addExperiment(data);
-		}
-	};
-
-	reader.readAsText(event.dataTransfer.files[0]);    
-	event.preventDefault();
-});
-
-window.addEventListener('dblclick', function(event){
-	var pos = ExperimentViewer.getClickedPanelPos();
-	ExperimentViewer.rotateToPos(pos);
-});
-
-window.addEventListener('mousedown', function(event){
-	if (event.button == 2) { 
-		ExperimentViewer.rotateToPos(ExperimentViewer.cameraPositions()["default"]);
+	tooltip(text){
+		this.tooltip.innerHTML = text;
 	}
-});
+}
 
 window.viewer = new ExperimentViewer();
 
